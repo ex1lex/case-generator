1c677cb224ac5519f8ac1bda967ea2aacb5b5b51
Alexey Obryadin <obryadin.alexey@gmail.com>
2023-11-24 16:58:24 +0300

    create app component

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import Card from './components/card';
+import './index.scss';
+
+const App: FC = () => {
+	return (
+		<div className="app">
+			<Card />
+		</div>
+	);
+};
+
+export default App;
